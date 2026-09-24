ec7d72ee471677595c3d121591159151a69b0e52
Shraddha Jain <shraddha04007@gmail.com>
2026-09-24 21:18:33 +0530

    updated student and faculty cordinators

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ruasLogo from './assets/ruas.png'
 import vectorLogo from './assets/vector.jpeg'
-import shrutiPhoto from './assets/shruti.jpeg'
-import samPhoto from './assets/sam.jpeg'
-import nikhilPhoto from './assets/nikhil.jpeg'
+import vectorupdatedlogo from './assets/vector-updated.jpeg'
 import remaPhoto from './assets/rema_mam.jpeg'
 import harshiniPhoto from './assets/Harshini.webp'
+import elvisphoto </elvis-meneze> from './assets/elvis-menezes.jpeg'
+import venkatphoto from './assets/venkat.jpeg'
+import ayushiphoto from './assets/ayushi.jpeg'
+import shinijineephoto from './assets/shinijinee.jpeg'
 
 // Event images imports
 import event1 from './assets/events/1.jpg'
@@ -176,18 +178,19 @@ function App() {
           </div>
           
           <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+
             {/* Member 1 */}
             <div className="bg-stone-800 border border-gray-600 rounded-xl p-8 text-center max-w-sm hover:bg-stone-700 transition-all duration-300">
               <img 
-                src={shrutiPhoto} 
-                alt="Shruthi Kannan" 
+                src={ venkatphoto } 
+                alt="Venkat" 
                 className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-purple-400"
               />
-              <h3 className="text-2xl font-semibold mb-2 text-purple-400">Shruthi Kannan</h3>
-              <p className="text-gray-400 mb-4">Core Member</p>
+              <h3 className="text-2xl font-semibold mb-2 text-purple-400">R Venketasubramaniaen</h3>
+              <p className="text-gray-400 mb-4">Student Head</p>
               <div className="flex justify-center space-x-4">
                 <a 
-                  href="https://www.linkedin.com/in/shruthi-kannan/" 
+                  href="https://www.linkedin.com/in/r-venkat-7a22963aa/" 
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="text-blue-400 hover:text-blue-300 transition-colors"
@@ -196,26 +199,22 @@ function App() {
                     <path fillRule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clipRule="evenodd" />
                   </svg>
                 </a>
-                <a href="#" className="text-gray-400 hover:text-gray-300 transition-colors">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
+              
               </div>
             </div>
 
             {/* Member 2 */}
             <div className="bg-stone-800 border border-gray-600 rounded-xl p-8 text-center max-w-sm hover:bg-stone-700 transition-all duration-300">
               <img 
-                src={samPhoto} 
-                alt="Samyuktha Pillai" 
-                className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-blue-400"
+                src={shinijineephoto} 
+                alt="Shinjinee  Bera" 
+                className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-yellow-400"
               />
-              <h3 className="text-2xl font-semibold mb-2 text-blue-400">Samyuktha Pillai</h3>
-              <p className="text-gray-400 mb-4">Core Member</p>
+              <h3 className="text-2xl font-semibold mb-2 text-yellow-400">Shinjinee  Bera</h3>
+              <p className="text-gray-400 mb-4">Treasure Head</p>
               <div className="flex justify-center space-x-4">
                 <a 
-                  href="https://www.linkedin.com/in/samyuktha-pillai-ba53752a1/" 
+                  href="https://www.linkedin.com/in/shinjinee-bera-b50ba9345/" 
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="text-blue-400 hover:text-blue-300 transition-colors"
@@ -224,41 +223,54 @@ function App() {
                     <path fillRule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clipRule="evenodd" />
                   </svg>
                 </a>
-                <a href="#" className="text-gray-400 hover:text-gray-300 transition-colors">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
+              
               </div>
             </div>
+
 
             {/* Member 3 */}
             <div className="bg-stone-800 border border-gray-600 rounded-xl p-8 text-center max-w-sm hover:bg-stone-700 transition-all duration-300">
               <img 
-                src={nikhilPhoto} 
-                alt="Nikhil Jha" 
+                src={  } 
+                alt="Bhuvan N Shan" 
+                className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-blue-400"
+              />
+              <h3 className="text-2xl font-semibold mb-2 text-blue-400">Bhuvan N Shan</h3>
+              <p className="text-gray-400 mb-4">Design Head</p>
+              <div className="flex justify-center space-x-4">
+                
+              </div>
+            </div>
+
+            
+
+            {/* Member 4 */}
+            <div className="bg-stone-800 border border-gray-600 rounded-xl p-8 text-center max-w-sm hover:bg-stone-700 transition-all duration-300">
+              <img 
+                src={ayushiphoto} 
+                alt="Ayushi" 
                 className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-yellow-400"
               />
-              <h3 className="text-2xl font-semibold mb-2 text-yellow-400">Nikhil Jha</h3>
-              <p className="text-gray-400 mb-4">Core Member</p>
+              <h3 className="text-2xl font-semibold mb-2 text-yellow-400">Ayushi</h3>
+              <p className="text-gray-400 mb-4">Logistics Head</p>
               <div className="flex justify-center space-x-4">
                 <a 
-                  href="https://www.linkedin.com/in/nikhil-jha-0424ab208/" 
+                  href="https://www.linkedin.com/in/ayushi-sinha-b2a885329/" 
                   target="_blank" 
                   rel="noopener noreferrer" 
-                  className="text-blue-400 hover:text-blue-300 transition-colors"
+                  className="text-yellow-400 hover:text-yellow-300 transition-colors"
                 >
                   <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clipRule="evenodd" />
                   </svg>
                 </a>
-                <a href="#" className="text-gray-400 hover:text-gray-300 transition-colors">
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
+                
               </div>
             </div>
+
+
+
+
           </div>
         </div>
       </section>
@@ -315,6 +327,31 @@ function App() {
               <div className="flex justify-center">
                 <a 
                   href="https://www.linkedin.com/in/harshini23/" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  className="text-blue-400 hover:text-blue-300 transition-colors"
+                >
+                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clipRule="evenodd" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+
+            {/* elvis menezes - Faculty Coordinator */}
+            <div className="bg-stone-700 border border-gray-500 rounded-xl p-8 text-center max-w-sm hover:bg-stone-600 transition-all duration-300">
+              <img 
+                src={elvisphoto} 
+                alt="Elvis Menezes" 
+                className="w-24 h-24 rounded-full mx-auto mb-6 object-cover border-4 border-cyan-400"
+              />
+              <h3 className="text-2xl font-semibold mb-2 text-cyan-400">Mr. Elvis Menezes</h3>
+              <p className="text-gray-300 mb-4 font-semibold">Faculty Coordinator</p>
+              <p className="text-gray-400 text-sm mb-4">Vector Data Science Club</p>
+              <div className="flex justify-center">
+                <a 
+                  href="https://www.linkedin.com/in/elvis-menezes-826758294/?trk=recent-searches" 
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="text-blue-400 hover:text-blue-300 transition-colors"
